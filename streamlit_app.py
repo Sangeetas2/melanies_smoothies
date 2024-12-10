0a44a537acf2e96781a6d9b0fc7a0f27583a02c7
Sangeetas2 <sangeetas2@hexaware.com>
2024-12-09 23:01:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,13 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect('choose upto 5 ingredients',my_dataframe,max_selections =5)
   
-if ingredients_list:
-    ingredients_string = ''
-    for fruit_choosen in ingredients_list:
-        ingredients_string +=fruit_choosen + ' '        
-    st.write(ingredients_string)
-
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+if  ingredients_list:
+        ingredients_string = ''
+        for fruit_choosen in ingredients_list:
+            ingredients_string +=fruit_choosen + ' '        
+        st.write(ingredients_string)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 #st.write(my_insert_stmt)
 time_to_insert = st.button('Submit Order')
