34a3914f107d3ceedd825dfeb48e2e7038039b97
Sangeetas2 <sangeetas2@hexaware.com>
2024-12-09 23:47:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 warehouse_sql = f"USE WAREHOUSE COMPUTE_WH"
 session.sql(warehouse_sql).collect()
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()                                                                      
                                                                       
