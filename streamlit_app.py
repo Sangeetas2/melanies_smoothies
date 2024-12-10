6ec8b2dee9950f672c1a5ee832435fe86f1cc3d5
Sangeetas2 <sangeetas2@hexaware.com>
2024-12-09 23:23:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ if time_to_insert:
     st.success(f"{'Your Smoothie is ordered,'}{name_on_order}{'!'}", icon="✅")
   
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
+#st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
