a33a9ac0ca12ce91e377ecb75b4c08d44b6d3cfc
Sangeetas2 <sangeetas2@hexaware.com>
2024-12-09 23:59:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,8 @@ warehouse_sql = f"USE WAREHOUSE COMPUTE_WH"
 session.sql(warehouse_sql).collect()
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-
-pd_fd = my_dataframe.to_pandas()
-#st.dataframe(pd_fd)
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 #st.stop()
                                                                           
 
